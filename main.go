a329636bf8080f3b09cf10224f520423f8405171
byte556 <subbotinsergej17@gmail.com>
2025-02-25 07:01:22 +0400

    main

--- main.go
+++ main.go
@@ -5,15 +5,20 @@ import (
 	"Blog/pkg/repository"
 	"Blog/pkg/service"
 	_ "modernc.org/sqlite"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT") // Получаем порт из окружения
+	if port == "" {
+		port = "8080" // Значение по умолчанию
+	}
 	db := repository.NewSQLite("sqlite", "./database.db")
 	rep := repository.NewRepository(db)
 	serv := service.NewService(rep)
 	handl := handler.NewHandler(serv)
 	engine := handl.InitRoutes()
-	err := engine.Run(":8080")
+	err := engine.Run(":" + port)
 	if err != nil {
 		return
 	}
